b30d65a7072a5cffe41421a806188722254dcc81
jeanmmlima <jeanmariomlima@gmail.com>
2022-01-20 23:56:10 -0300

    first commit exemplo jsf loja

---
+++ loja/src/main/java/com/aulas/loja/App.java
@@ -0,0 +1,13 @@
+package com.aulas.loja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ loja/src/main/java/com/aulas/loja/dominio/Usuario.java
@@ -0,0 +1,79 @@
+package com.aulas.loja.dominio;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 30, nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	public Usuario(String nome, String senha) {
+		super();
+		this.nome = nome;
+		this.senha = senha;
+	}
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome, senha);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome) && Objects.equals(senha, other.senha);
+	}
+	
+	
+	
+	
+
+}
